ad59268871f92ae9ed797cae20de09e2f9c79b86
Ioan George Istrate <istrate_ioan_george@yahoo.com>
2014-10-28 16:02:01 -0400

    nodes config

--- HashTable.h
+++ HashTable.h
@@ -6,13 +6,38 @@
 class HashTable {
 private:
 	const int m_size;
-	struct Node {
+	class Node {
+	private:
 		int m_key;
 		string m_value;
 		Node* m_next;
+	public:
+		Node() : m_key(0), m_value(0), m_next(0) {}
+		Node(int key) : m_key(key), m_value(0), m_next(0) {}
+		Node(int key, string value) : m_key(key), m_value(value), m_next(0) {}
+
+		void setNext(Node* next) {
+			m_next = next;
+		}
+		void setIndex(int key) {
+			m_key = key;
+		}
+		void setValue(string value) {
+			m_value = value;
+		}
+
+		Node* getNext(void) {
+			return m_next;
+		}
+		int getIndex(void) {
+			return m_key;
+		}
+		string getValue(void) {
+			return m_value;
+		}
 	};
 public:
-	HashTable() : m_size(100) {}
+	HashTable() : m_size(25) {}
 	int hash(string value);
 };
 
--- SpellChecker.cpp
+++ SpellChecker.cpp
@@ -1,7 +1,6 @@
 #include "SpellChecker.h"
 
 void SpellChecker::run(void) {
-	int test = m_HT.hash("abc");
-	cout << test << endl;
-	m_Debugger.pTest("Index is" + test);
+	int test = m_HT.hash("aardwolves");
+	m_Debugger.pTest("Index is " + std::to_string(test));
 }
